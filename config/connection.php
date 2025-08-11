efd6bc5588da59c18cd58e5b7cc76628b79fb205
Breno Mariano <bre.mariano0@gmail.com>
2025-08-11 18:07:27 -0300

    creating database and its connection

---
+++ config/connection.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = "localhost";
+$dbname = "agenda";
+$user = "root";
+$pass = "";
+
+try {
+
+    $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+}catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
